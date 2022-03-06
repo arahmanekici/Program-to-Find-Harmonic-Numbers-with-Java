b459c77dab5984a4bd578927c46bebd883f8ce96
SuperCat <arahmanekici@hotmail.com>
2022-03-06 12:33:33 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    static public void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        double number = input.nextDouble();
+        double total= 0;
+        double number1=0;
+
+        for (int i=1; i<=number; i++){
+
+            number1= 1.0/i;
+            total+= number1;
+
+
+        }
+        System.out.println(total);
+    }
+}
